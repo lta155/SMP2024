66bfcc1224d1d73b1bc7568c0654e9671f96e33c
yws-coder123 <167846644+yws-coder123@users.noreply.github.com>
2024-09-16 10:39:14 +0800

    修改并发

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import os
 
-import agentops
+from autogen import Cache
 import dotenv
 from tqdm import tqdm
 
@@ -10,24 +10,25 @@ from gpt4o import *
 dotenv.load_dotenv()
 
 file_name="SMP_240915_answer_1"
+# sign=True
 def run(id_and_content: str):
     cache_seed = 1
     file_name = "SMP_240915_answer_1"
     ID, content = id_and_content.split("@####@")
-    agentops.init(tags=["ID:" + ID,file_name])
+    # agentops.init(auto_start_session=False)
+    # agentops.start_session(tags=[file_name, "ID:"+ID])
     try:
-
         # Use DiskCache as cache
-    # with Cache.disk(cache_path_root="./autogen_cache", cache_seed=cache_seed) as cache:
-        chat_result = code_executor_agent.initiate_chat(
-            code_writer_agent,
-            message=content,
-            summary_method='reflection_with_llm',
-            summary_args=dict(summary_prompt='only return the code output'),
-            # cache=cache,
-            silent=True,
-        )
-        agentops.end_session('Success')
+        with Cache.disk(cache_path_root="./autogen_cache", cache_seed=cache_seed) as cache:
+            chat_result = code_executor_agent.initiate_chat(
+                code_writer_agent,
+                message=content,
+                summary_method='reflection_with_llm',
+                summary_args=dict(summary_prompt='only return the code output'),
+                cache=cache,
+                silent=True,
+            )
+        # agentops.end_session('Success')
         code = ""
         for i in range(len(chat_result.chat_history) - 1, 0, -1):
             l = extract_python_code(chat_result.chat_history[i]['content'])
@@ -41,15 +42,14 @@ def run(id_and_content: str):
         # item['chat_history']=chat_result.chat_history
         return ID+"@####@"+code+"@####@"+answer
     except Exception as e:
-        agentops.end_session('Failure')
+        # agentops.end_session('Failure')
         print(f"Error processing item {ID}: {str(e)}")
-        return f"Error processing item {ID}: {str(e)}"
+        return ID+"@####@"+f"Error processing item {ID}: {str(e)}"
 
 def run_concurrent(items):
     results = []
     try:
         with multiprocessing.Pool(processes=10) as pool:
-            # 使用 `tqdm` 显示进度条
             for result in tqdm(pool.imap_unordered(run, items), total=len(items)):
                 if result is not None:
                     results.append(result)  # 仅记录成功的任务结果
@@ -62,38 +62,60 @@ def run_concurrent(items):
 
 
 if __name__ == "__main__":
+    FROM,TO=0,512
     with open('data/Final_TestSet/Final_TestSet.json', 'r', encoding='utf-8') as f:
-        dataset = json.load(f)
-    with open('data/Final_TestSet/id_and_content.json', 'r', encoding='utf-8') as f:
-        id_and_content = json.load(f)
+        dataset = json.load(f)[:]
+    with open('data/id_and_content_0916_1.json', 'r', encoding='utf-8') as f:
+        id_and_content = json.load(f)[:TO]
     # 判断文件存在
-    if os.path.exists(f'data/Final_TestSet/{file_name}.json'):
-        with open(f'data/Final_TestSet/{file_name}.json', 'r', encoding='utf-8') as f:
-            answers=json.load(f)
+    if os.path.exists(f'data/{file_name}.json'):
+        with open(f'data/{file_name}.json', 'r', encoding='utf-8') as f:
+            answers=json.load(f)[:TO]
     else:
         answers=[]
 
     print("预处理")
-    _id=[i["ID"] for i in answers if "answer" in i]
-    id_and_content=[i for i in id_and_content if i.split("@####@")[0] not in _id]
+    _id=[i["ID"] for i in answers if "answer" in i and i["answer"]!=""]
+    id_and_content=[str(i["ID"] )+"@####@"+i["content"] for i in id_and_content if i["ID"] not in _id]
 
     print(f"运行,共{len(id_and_content)}")
     id_and_code_and_answer=run_concurrent(id_and_content)
-    id_and_code_and_answer = sorted([i.split("@####@") for i in id_and_code_and_answer], key=lambda x: x[0],
-                                    reverse=False)
-    print("验证")
-    for i in range(len(dataset)):
-        assert dataset[i]["ID"] == id_and_code_and_answer[i][0]
+    id_and_code_and_answer_json=[]
+    for item in id_and_code_and_answer:
+        tmp=item.split("@####@")
+        if len(tmp)==3:
+            id_and_code_and_answer_json.append({
+                "ID": int(tmp[0]),
+                "code": tmp[1],
+                "answer": tmp[2]
+            })
+        else:
+            id_and_code_and_answer_json.append({
+                "ID": int(tmp[0]),
+                "code": "",
+                "answer": ""
+            })
+    id_and_code_and_answer_json=sorted(id_and_code_and_answer_json, key=lambda x: x["ID"], reverse=False)
+
+
+    print("添加结果")
+    final_answer=[]
+    for dataset_item in dataset:
+        tmp={"ID": dataset_item["ID"], "question":dataset_item["question"],"code":"","answer":""}
+        for old_answer in answers:
+            if old_answer["ID"]==dataset_item["ID"]:
+                tmp["code"]=old_answer["code"]
+                tmp["answer"]=old_answer["answer"]
+
+        for new_answer in id_and_code_and_answer_json:
+            if new_answer["ID"]==dataset_item["ID"]:
+                tmp["code"]=new_answer["code"]
+                tmp["answer"]=new_answer["answer"]
+                print("update:",new_answer["ID"])
+        final_answer.append(tmp)
 
     print("存储")
-    new_dataset=[]
-    for i in range(len(dataset)):
-        new_dataset.append({
-            "ID": dataset[i]["ID"],
-            "code": id_and_code_and_answer[i][1],
-            "answer": id_and_code_and_answer[i][2]
-        })
     with open(f'data/{file_name}.json', 'w', encoding='utf-8') as f:
-        s = json.dumps(new_dataset, indent=4, ensure_ascii=False)
+        s = json.dumps(final_answer, indent=4, ensure_ascii=False)
         f.write(s)
 
